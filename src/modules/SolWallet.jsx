3417a33ca98662fbe9aeccc8c324f78bebf3579e
junaid2415 <junaidmohammad2415@gmail.com>
2024-08-21 08:17:15 +0530

    sol wallet added

---
+++ src/modules/SolWallet.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SolWallet() {
+  return (
+    <div>SolWallet</div>
+  )
+}
--- src/modules/index.jsx
+++ src/modules/index.jsx
@@ -4,6 +4,7 @@ import nacl from "tweetnacl";
 import { generateMnemonic, mnemonicToSeedSync } from "bip39";
 import { derivePath } from "ed25519-hd-key";
 import { Keypair } from "@solana/web3.js";
+import axios from 'axios';
 
 window.Buffer = Buffer;
 
@@ -11,6 +12,7 @@ export default function Index() {
 
   const [mnemonic, setMnemonic] = useState('')
   const [seed, setSeed] = useState('')
+  const [balance, setBalance] = useState('')
   const [disabled, setDisabled] = useState(false)
   const [solWallets, setSolWallets] = useState([])
   const [ethWallets, setEthWallets] = useState([])
@@ -46,16 +48,65 @@ export default function Index() {
     setEthWallets([...ethWallets, publicKey])
   }
 
+  async function checkBalanceSol(publicKey){
+
+    const rpcUrl = `https://devnet.helius-rpc.com/?api-key=bf48427a-6612-48d8-97ce-b5cb4a7c054d`
+    const body = {
+      "jsonrpc": "2.0",
+      "id": 1,
+      "method": "getBalance",
+      "params": [`${publicKey}`]
+    }
+
+    let response  = await axios.post(rpcUrl, body)
+    let balance = response?.data?.result?.value
+    console.log(balance)
+    setBalance(balance)
+    return balance
+    
+  }
+
+  async function checkBalanceEth(publicKey){
+
+    const rpcUrl = `https://devnet.helius-rpc.com/?api-key=bf48427a-6612-48d8-97ce-b5cb4a7c054d`
+    const body = {
+      "jsonrpc": "2.0",
+      "id": 1,
+      "method": "eth_getBalance",
+      "params": [`${publicKey}`, "latest"]
+  }
+
+    let response  = await axios.post(rpcUrl, body)
+    let balance = response?.data?.result?.value
+    console.log(balance)
+    setBalance(balance)
+    return balance
+
+  }
+
   return (
     <div>
       <h3>Generate Mnemonics</h3>
       <h2>{mnemonic}</h2>
-      <h2>{seed}</h2>
+      {/* <h2>{seed}</h2> */}
       <button onClick={generateMnemonicLocal} disabled={disabled}>Generate</button>
+      <h3></h3>
+      <button onClick={() => checkBalanceSol(solWallets[0])}>Check Balance SOL</button>
+
+      <h3>{balance}</h3>
+
+
+      <h3></h3>
+      <button onClick={() => checkBalanceEth(ethWallets[0])}>Check Balance ETH</button>
+
+      <h3>{balance}</h3>
+
       
+
       <button onClick={() => addSolWallet()}>Add Sol Wallet</button>
       <h2>Sol Wallets</h2>
       {solWallets.map(obj => <h3>{obj}</h3>)}
+
       <button onClick={addEthWallet}>Add Eth Wallet</button>
       <h3>Eth Wallets</h3>
       {ethWallets.map(obj => <h3>{obj}</h3>)}
